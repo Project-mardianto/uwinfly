6d60f94b8701211a4a2d973e6e4201a2fe69250c
Mardianto <96780977+Project-mardianto@users.noreply.github.com>
2024-03-19 12:13:24 +0000

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,8 +32,6 @@ function Home() {
         query={useRouter()?.query}
       >
         <PlasmicHome />
-        <script async src="https://cse.google.com/cse.js?cx=a57e7f63f92bd473d">
-        </script>
       </PageParamsProvider__>
     </GlobalContextsProvider>
   );
