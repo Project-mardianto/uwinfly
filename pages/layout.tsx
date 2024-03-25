e13bfb3c1257fd7eaeff454c9c8f5586b23cfcfb
Mardianto <96780977+Project-mardianto@users.noreply.github.com>
2024-03-25 21:26:30 +0700

    Update layout.tsx

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+     <script type='text/javascript' id='pagemapIdscript' defer src='https://bot.linkbot.com/bot.js#35194-37352?property-id=35194-37352'></script>
       <script async src="https://cse.google.com/cse.js?cx=a57e7f63f92bd473d"></script>
         <div className="gcse-search"></div>
       <GoogleTagManager gtmId="AW-16491756846" />
